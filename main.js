79bd7c05adaa593ae3ea0c256e51ad7e69c92cd2
mursalin2250 <87318671+mursalin2250@users.noreply.github.com>
2024-03-20 14:10:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function Showbtn(){
+    document.getElementById("pass").type="text";
+    document.getElementById("show").style.display="none";
+    document.getElementById("hide").style.display="inline";
+
+}
+function Hidebtn(){
+    document.getElementById("pass").type="password";
+    document.getElementById("show").style.display="inline";
+    document.getElementById("hide").style.display="none";
+}
+function message(){
+    window.alert("Sorry abo login button active korsi na :)")
+}

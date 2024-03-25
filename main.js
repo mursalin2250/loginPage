c342dd4b49bd41c1f95b777007496b7c669039e1
mursalin2250 <87318671+mursalin2250@users.noreply.github.com>
2024-03-26 04:46:18 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function Showbtn(){
-    document.getElementById("pass").type="text";
+    pswd = document.getElementById("pass").type="text";
     document.getElementById("show").style.display="none";
     document.getElementById("hide").style.display="inline";
 
@@ -9,6 +9,37 @@ function Hidebtn(){
     document.getElementById("show").style.display="inline";
     document.getElementById("hide").style.display="none";
 }
-function message(){
-    window.alert("Sorry abo login button active korsi na :)")
+function show(){
+    pswd = document.getElementById("confirm").type="text";
+    document.getElementById("showbtn").style.display="none";
+    document.getElementById("hidebtn").style.display="inline";
+
 }
+function hide(){
+    document.getElementById("confirm").type="password";
+    document.getElementById("showbtn").style.display="inline";
+    document.getElementById("hidebtn").style.display="none";
+}
+function eye(){
+    document.getElementById('show').style.display = "inline";
+}
+function Eye(){
+    document.getElementById('showbtn').style.display="inline";
+}
+function message(){
+    var pswd = document.getElementById('pass').value;
+    var confirm =document.getElementById("confirm").value;
+    var email =document.getElementById("email").value;
+    if(email == "" || pswd == "" || confirm == ""){
+        window.alert("Please enter the credentials correctly.");
+    }
+    else if(pswd == confirm){
+        alert("Congratulations! Account created successfully.");
+    }
+    else if(pswd != confirm){
+        window.alert("Passwords did not match. Please try again.");
+    }
+    else{
+        alert("Please enter the credentials correctly.")
+    }
+};
